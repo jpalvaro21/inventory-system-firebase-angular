30f839c3822dfc0023ccca19da137e538f30c2e6
John Paul Alvaro <John_Paul_Pastor_Alvaro@manulife.com>
2019-05-06 15:59:51 +0800

    Update to support bundled prices

--- src/app/core/inventory-dialog/inventory-dialog.component.ts
+++ src/app/core/inventory-dialog/inventory-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA, MatSnackBar } from '@angular/material';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 import { InventoryService } from '../services/inventory.service';
 import { Inventory } from '../model/inventory';
 
@@ -28,10 +28,37 @@ export class InventoryDialogComponent implements OnInit {
     this.inventoryDialogForm = this.fb.group({
       name: new FormControl(null, Validators.required),
       brand: new FormControl(null),
-      price: new FormControl(0, Validators.min(.25)),
       quantity: new FormControl(0, Validators.required),
-      additionalInfo: new FormControl(null)
+      additionalInfo: new FormControl(null),
+      bundleInfo: new FormArray([
+        this.createBundleInfoForm()
+      ])
     });
+  }
+
+  get bundleInfo(): FormArray {
+    return this.inventoryDialogForm.get('bundleInfo') as FormArray;
+  }
+
+  private createBundleInfoForm(quantityDefault: number = 1, priceDefault: number = 0): FormGroup {
+    return this.fb.group({
+      price: new FormControl(priceDefault, Validators.min(0.25)),
+      quantity: new FormControl(quantityDefault, Validators.min(1))
+    });
+  }
+
+  addBundle() {
+    let highestQuantity = 0;
+
+    this.bundleInfo.controls.forEach((bundle: FormGroup) => {
+      highestQuantity = Math.max(bundle.get('quantity').value, highestQuantity);
+    });
+
+    this.bundleInfo.push(this.createBundleInfoForm(highestQuantity + 1));
+  }
+
+  removeBundle(index: number) {
+    this.bundleInfo.controls.splice(index, 1);
   }
 
   cancel() {
--- src/app/core/model/inventory.ts
+++ src/app/core/model/inventory.ts
@@ -5,6 +5,11 @@ export class Inventory extends FirestoreDocument {
   name: string;
   brand?: string;
   additionalInfo: string;
+  quantity: number;
+  bundleInfo: Array<Bundle> = [];
+}
+
+export class Bundle {
   price: number;
   quantity: number;
 }
